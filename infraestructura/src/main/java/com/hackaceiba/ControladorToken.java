1eaf756dcebd59cd1b943d85a4f2c32a5a59f7c4
Fabio Andrés Vásquez Alvarez <fabio.vasquez@ceiba.com.co>
2021-11-12 20:42:21 -0500

    Depuración archivo configuración y controlador inicial

--- infraestructura/src/main/java/com/hackaceiba/ControladorToken.java
+++ infraestructura/src/main/java/com/hackaceiba/ControladorToken.java
@@ -1,5 +1,15 @@
 package com.hackaceiba;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
 public class ControladorToken {
 
+	@GetMapping
+	public String test() {
+		return "El número ingresado es: " + 2;
+	}
 }
