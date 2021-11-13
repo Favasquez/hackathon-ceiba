0845e2652f1b0a44addffbce38a08340b2d3bef3
maria.molina <maria.molina@ceiba.com.co>
2021-11-13 08:34:28 -0500

    Se realiza configuracion de docker

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,7 @@
 FROM openjdk:11-jre-slim
- 
+RUN apt-get update && apt-get install -y tzdata openssl
+ENV TZ America/Bogota
+
+COPY build/libs/hackaceiba-1.0-SNAPSHOT.jar hackaceiba-1.0-SNAPSHOT.jar
+
+CMD java -jar hackaceiba-1.0-SNAPSHOT.jar
--- infraestructura/src/main/java/com/hackaceiba/configuracion/ConfiguracionClienteWeb.java
+++ infraestructura/src/main/java/com/hackaceiba/configuracion/ConfiguracionClienteWeb.java
@@ -3,10 +3,7 @@ package com.hackaceiba.configuracion;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.netty.http.client.HttpClient;
 
 @Profile("!test")
 @Configuration
